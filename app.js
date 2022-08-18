3d6753cd315a800b06abb04870702cf5e8cd8875
Kavishka <kavishkaprabath21@gmail.com>
2022-08-18 20:47:41 +0530

    customer, item and order requests send

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+app.get('/',(req,res) =>{
+    console.log('get request coming!');
+    res.send('get req came for / route')
+});
+
+app.get('/customer',(req,res) =>{
+    console.log('get request coming!');
+    res.send('<h1>customer get request came <h1/>')
+});
+
+app.get('/item',(req,res) =>{
+    console.log('get request coming!');
+    res.send('<h1>item get request came <h1/>')
+});
+
+app.get('/order',(req,res) =>{
+    console.log('get request coming!');
+    res.send('<h1>order get request came <h1/>')
+});
+
 app.listen(port, () =>{
-    console.log(`app listening on port ${port}`)
-})
+    console.log(`app listening on port ${port}`);
+});
