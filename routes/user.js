9dafa4eeeb090aebbe05b244ca4f7f57642b5074
Kavishka <kavishkaprabath21@gmail.com>
2022-08-18 21:07:15 +0530

    add user get, post, put and delete method

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const db = require('../configs/db.config')
 
 const connection = mysql.createConnection(db.database)
 
+// user connect to db
 connection.connect(function(err){
     if(err){
         console.log(err);
@@ -21,3 +22,70 @@ connection.connect(function(err){
 })
 
 const router = express.Router()
+
+// get user
+router.get('/',(req, res) =>{
+    var query = "SELECT * FROM users"
+
+    connection.query(query,(err,rows) =>{
+        if(err) throw err
+
+        res.send(rows)
+    })
+})
+
+// save user
+router.post('/',(req, res) =>{
+    const id = req.body.id
+    const name = req.body.name
+    const username = req.body.username
+
+    var query = "INSERT INTO users (id, name, username) VALUES (?,?,?)"
+
+    connection.query(query, [id, name, username], (err) =>{
+        if(err){
+            res.send({"message" : "duplicate entry"})
+        }else{
+            res.send({"message" : "user created!"})
+        }
+    })
+})
+
+// update user
+router.put('/',(req, res) =>{
+    const id = req.body.id
+    const name = req.body.name
+    const username = req.body.username
+
+    var query = "UPDATE users SET name =?, username=? WHERE id=?"
+
+    connection.query(query, [name, username, id], (err,rows) =>{
+        if(err) console.log(err);
+
+        if(rows.affectedRows > 0){
+            res.send({'message' : 'User Updated'})
+        }else{
+            res.send({'message' : 'User not found'})
+        }
+
+    })
+})
+
+// delete user
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+
+    var query = "DELETE FROM users WHERE id=?";
+
+    connection.query(query, [id], (err, rows) => {
+        if (err) console.log(err);
+
+        if (rows.affectedRows > 0) {
+            res.send({ 'message': 'user deleted' })
+        } else {
+            res.send({ 'message': 'user not found' })
+        }
+    })
+})
+
+module.exports = router;
